3e18f29a9e76f6d1ae0ec0ae3530d988e2bb7439
Brandon Dube <brandondube@gmail.com>
2017-10-11 00:03:24 -0400

    refine legend and axis ticks handling

--- pyphase/liveplot.py
+++ pyphase/liveplot.py
@@ -22,6 +22,7 @@ with Listener(address, authkey=b'pyphase_live') as listener:
         plt_lines = None
         iters = []
         iters_current = 0
+        ymax_last = 0
         while True:
             msg = conn.recv()
             if msg == 'quit':
@@ -68,11 +69,17 @@ with Listener(address, authkey=b'pyphase_live') as listener:
                 for idx in range(len(msg)):
                     line, = ax.plot([], [], lw=line_width)
                     plt_lines.append(line)
-                
-                print(len(plt_lines))
             
-            if plt_add_legend:
-                ax.legend(plt_labels)
+            if plt_add_legend and plt_data is not None:
+                ncols = 1
+                if len(plt_labels) > 6:
+                    ncols = 2
+                elif len(plt_labels > 9):
+                    ncols = 3
+                elif len(plt-labels > 12):
+                    ncols = 4
+                ax.legend(plt_labels, ncol=ncols)
+                plt_add_legend = False
             
             iters.append(iters_current)
             iters_current += 1
@@ -87,4 +94,10 @@ with Listener(address, authkey=b'pyphase_live') as listener:
             ax.relim()
             # update ax.viewLim using the new dataLim
             ax.autoscale_view()
+
+            # avoid overflow from axis limits
+            maxs = [max(vector) for vector in plt_data]
+            ymax = max(maxs)
+            if len(str(ymax)) > len(str(ymax_last)):
+                fig.tight_layout()
             plt.pause(0.0001)
