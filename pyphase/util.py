b4d15753eb8d89a0705daa5bb440084be5f64d70
Brandon Dube <brandondube@gmail.com>
2017-11-19 15:29:40 -0500

    function to rount to integers

--- pyphase/util.py
+++ pyphase/util.py
@@ -3,15 +3,6 @@
 import numpy as np
 import pandas as pd
 
-from prysm.util import share_fig_ax, correct_gamma
-
-parameters = {
-    'efl': 50,
-    'fno': 2,
-    'wavelength': 0.5,
-    'samples': 128
-}
-
 def mtf_cost_fcn(true_tan, true_sag, sim_tan, sim_sag):
     ''' A cost function that compares a measured or simulated T/S MTF to a
         simulated one.
@@ -19,3 +10,6 @@ def mtf_cost_fcn(true_tan, true_sag, sim_tan, sim_sag):
     t = ((true_tan-sim_tan)**2).sum()
     s = ((true_sag-sim_sag)**2).sum()
     return t+s
+
+def round_to_int(value, integer):
+    return integer * round(float(value)/integer)
