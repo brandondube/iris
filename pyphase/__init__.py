147cadb277f9b59b778d76ffcda584f172390a49
Brandon Dube <brandondube@gmail.com>
2017-11-19 15:30:43 -0500

    import/export mtf_cost_fcn

--- pyphase/__init__.py
+++ pyphase/__init__.py
@@ -2,6 +2,11 @@ from pyphase.core import (
     thrufocus_mtf_from_wavefront,
 )
 
+from pyphase.util import (
+    mtf_cost_fcn,
+)
+
 __all__ = [
     'thrufocus_mtf_from_wavefront',
+    'mtf_cost_fcn',
 ]
