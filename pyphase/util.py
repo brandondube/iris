9b8908fde6c7d7bfb378027d5cc2bf98bb330ae4
Brandon Dube <brandondube@gmail.com>
2017-11-23 01:42:06 -0500

    parser to get cost function per iter from fortran prints

--- pyphase/util.py
+++ pyphase/util.py
@@ -1,7 +1,6 @@
 ''' Misc. utilities
 '''
-import numpy as np
-import pandas as pd
+import re
 
 def mtf_cost_fcn(true_tan, true_sag, sim_tan, sim_sag):
     ''' A cost function that compares a measured or simulated T/S MTF to a
@@ -13,3 +12,15 @@ def mtf_cost_fcn(true_tan, true_sag, sim_tan, sim_sag):
 
 def round_to_int(value, integer):
     return integer * round(float(value)/integer)
+
+def parse_cost_by_iter_lbfgsb(string):
+    # use regex to find lines that include "at iteration" information.
+    lines_with_cost_function_values = \
+        re.findall(r'At iterate\s*\d\s*f=\s*-*?\d*.\d*D[+-]\d*', string)
+
+    # grab the value from each line and convert to python floats
+    fortran_values = [s.split()[-1] for s in lines_with_cost_function_values]
+    # fortran uses "D" to denote double and "raw" exp notation,
+    # fortran value 3.0000000D+02 is equivalent to
+    # python value  3.0000000E+02 with double precision
+    return [float(s.replace('D', 'E')) for s in fortran_values]
