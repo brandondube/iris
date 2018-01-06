3d940544f645ee28cca4a23818e0b0b87fc2c71c
Brandon Dube <brandondube@gmail.com>
2018-01-06 13:20:47 -0500

    fully functional* persistent work queues

--- pyphase/persistent_queue.py
+++ pyphase/persistent_queue.py
@@ -5,7 +5,24 @@ from collections import deque
 
 
 class PersistentQueue(deque):
+    ''' A disk-persistent queue
 
+    Notes:
+        This object is not thread-safe and will have unexpected behavior if
+        shared between threads.
+
+        The intended / expected behavior is as follows:
+            - put() adds items to the end of the queue and persists it to disk.
+
+            - the user will use peek() to nondestructively retrieve the top item
+                on the queue.  When a task using that data is complete,
+                mark_done() will be called, which will persist the queue to disk.
+
+            - use of get() destructively returns the first item from the queue and
+                persists it to disk.  Use this where the consuming code of that
+                object will not fail.
+
+    '''
     def __init__(self, path):
         ''' Creates a new PersistentQueue, a deque-based object which functions
             like a FIFO queue and caches to disk on each call.  The queue is not
@@ -18,31 +35,18 @@ class PersistentQueue(deque):
         Returns:
             PersistentQueue: a new persistent queue.
 
-        Notes:
-            This object is not thread-safe and will have unexpected behavior if
-            shared between threads.
-
-            The intended / expected behavior is as follows:
-                - put() adds items to the end of the queue and persists it to
-                    disk.
-
-                - the user will use peek() to nondestructively retrieve the top
-                    item on the queue.  When a task using that data is complete,
-                    mark_done() will be called, which will persist the queue to
-                    disk.
-
-                - use of get() destructively returns the first item from the
-                    queue and persists it to disk.  Use this where the consuming
-                    code of that object will not fail.
-
         '''
 
         self.path = path
-        self.q = deque()
+        try:
+            with open(self.path, mode='rb') as file:
+                self.q = pickle.load(file)
+        except (FileNotFoundError, IOError):
+            self.q = deque()
 
     def persist(self):
-        with open(self.path, mode='b', encoding='utf-8') as file:
-            pickle.dump(self, file, protocol=-1)
+        with open(self.path, mode='wb') as file:
+            pickle.dump(self.q, file, protocol=-1)
 
     def put(self, item):
         self.q.append(item)
