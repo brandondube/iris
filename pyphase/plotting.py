25e350b5523cb378bd3b1152fd3f2aecbfb075de
Brandon Dube <brandondube@gmail.com>
2018-01-06 16:28:58 -0500

    log scale capability + paper template

--- pyphase/plotting.py
+++ pyphase/plotting.py
@@ -1,16 +1,20 @@
 ''' Tools for plotting the results of phase retrieval
 '''
 import numpy as np
-import matplotlib as mpl
 from matplotlib import pyplot as plt
 plt.style.use('ggplot')
-mpl.rcParams.update({'lines.linewidth': 3})
 
-def single_solve_quad(result_document, type='axis_sph', fig=None, axs=None):
+
+def single_solve_triple(result_document, type='axis_sph', log=False, fig=None, axs=None):
     ''' Plots a quadchart for a single solve, giving full diagnostic information
         into the result.
 
     '''
+    if log is False:
+        scale = 'linear'
+    else:
+        scale = 'log'
+
     # pull the relevant data
     rd = result_document
     params, cost, rmswfe = rd['x_iter'], rd['fun_iter'], rd['rmswfe_iter']
@@ -36,12 +40,49 @@ def single_solve_quad(result_document, type='axis_sph', fig=None, axs=None):
     axs[1].plot(iters, cost)
     axs[1].set(xlabel='Iteration [-]',
                ylabel='Value [-]',
-               title='Cost Function')
+               yscale=scale,
+               title='Cost Function',)
 
     axs[2].plot(iters, rmswfe)
     axs[2].set(xlabel='Iteration [-]',
                ylabel=r'Residual RMS WFE [$\lambda$]',
+               yscale=scale,
                title='RMS WFE')
+
+    fig.tight_layout()
+    return fig, axs
+
+
+def single_solve_paper(result_document, fig=None, axs=None):
+    ''' Plots the convergence of parameters on a symmetric log scale
+    '''
+
+    rd = result_document
+    truth = rd['truth_zernike']
+    truths = np.asarray(list(truth.values()), dtype=np.float64)
+    params, rmswfe = rd['x_iter'], rd['rmswfe_iter']
+    iters = list(range(len(params)))
+    params = np.asarray(params)
+    p_shape = params.shape
+
+    # prepare the plot
+    fig, axs = plt.subplots(ncols=2, sharex=True, figsize=(8, 3.75))
+
+    # get the parameter names
+    names = list(result_document['retrieved_zernike'].keys())
+    truths = list(result_document['truth_zernike'].values())
+    for i, name, truth in zip(range(p_shape[1]), names, truths):
+        line, = axs[0].plot(iters, params[:, i], label=f'{name} : {truth}')
+        axs[0].scatter(iters[-1], truth, c=line.get_color(), linewidths=2.5)
+
+    axs[0].legend()
+    axs[0].set(xlabel='Optimizer Iteration',
+               ylabel=r'Zernike Weight [$\lambda$ 0-P]')
+
+    axs[1].plot(iters, rmswfe, c='0.25')
+    axs[1].set(xlabel='Optimizer Iteration',
+               ylabel=r'Residual RMS WFE [$\lambda$]',
+               yscale='log')
 
     fig.tight_layout()
     return fig, axs
