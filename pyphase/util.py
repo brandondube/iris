e7dcf7798e2cfde1ca2373cbd09264771dbd9da0
Brandon Dube <brandondube@gmail.com>
2018-01-06 17:25:42 -0500

    linting

--- pyphase/util.py
+++ pyphase/util.py
@@ -2,16 +2,19 @@
 '''
 import re
 
+
 def mtf_cost_fcn(true_tan, true_sag, sim_tan, sim_sag):
     ''' A cost function that compares a measured or simulated T/S MTF to a
         simulated one.
     '''
-    t = ((true_tan-sim_tan)**2).sum()
-    s = ((true_sag-sim_sag)**2).sum()
-    return t+s
+    t = ((true_tan - sim_tan) ** 2).sum()
+    s = ((true_sag - sim_sag) ** 2).sum()
+    return t + s
+
 
 def round_to_int(value, integer):
-    return integer * round(float(value)/integer)
+    return integer * round(float(value) / integer)
+
 
 def parse_cost_by_iter_lbfgsb(string):
     # use regex to find lines that include "at iteration" information.
