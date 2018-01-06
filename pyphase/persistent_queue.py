c50800c2a0cd14d8f9e546cc6e2ad0973b70d290
Brandon Dube <brandondube@gmail.com>
2018-01-06 13:12:52 -0500

    queue complete except restore from disk

--- pyphase/persistent_queue.py
+++ pyphase/persistent_queue.py
@@ -6,43 +6,57 @@ from collections import deque
 
 class PersistentQueue(deque):
 
-    def __init__(self, write_path, write_time=None, write_on_action=True, maxsize=0):
-        ''' Creates a new PersistentQueue, a Queue object which persists to disk
-            at an interval determined by write_time or write_on_action.
+    def __init__(self, path):
+        ''' Creates a new PersistentQueue, a deque-based object which functions
+            like a FIFO queue and caches to disk on each call.  The queue is not
+            thread safe and internally uses two deques to support safe failure
+            upon consumption of a piece of the queue.
 
         Args:
-            write_path (`str`): where to persist the queue too.
-
-            write_time (`number` or `None`): if is None, does not periodically
-                write.  If a number, saves to disk asyncronously every `number`
-                seconds.  Note that this mode of operation is not currently used.
-
-            write_on_action (`bool`): whether to write on actions or not.  If
-                this value is True, write_time will not be used.
-
-            maxsize (`int`): maxsize, passed on to the Queue constructor.
+            path (`str`): where to persist the queue too.
 
         Returns:
             PersistentQueue: a new persistent queue.
 
+        Notes:
+            This object is not thread-safe and will have unexpected behavior if
+            shared between threads.
+
+            The intended / expected behavior is as follows:
+                - put() adds items to the end of the queue and persists it to
+                    disk.
+
+                - the user will use peek() to nondestructively retrieve the top
+                    item on the queue.  When a task using that data is complete,
+                    mark_done() will be called, which will persist the queue to
+                    disk.
+
+                - use of get() destructively returns the first item from the
+                    queue and persists it to disk.  Use this where the consuming
+                    code of that object will not fail.
+
         '''
-        if write_on_action is True:
-            self.write_on_action = True
-            self.write_time = None
-        elif write_time is not None:
-            self.write_on_action = False
-            self.write_time = write_time
 
-        self.path = write_path
-
-        super().__init__(maxsize)
+        self.path = path
+        self.q = deque()
 
     def persist(self):
         with open(self.path, mode='b', encoding='utf-8') as file:
             pickle.dump(self, file, protocol=-1)
 
-    def put(self, item, block=True, timeout=None):
-        super().put(item, block, timeout)
+    def put(self, item):
+        self.q.append(item)
         self.persist()
 
-    def get(self, block=True, timeout=None)
+    def get(self):
+        return self.q.popleft()
+        self.persist()
+
+    def peek(self):
+        item = self.q.popleft()
+        self.q.appendleft(item)
+        return item
+
+    def mark_done(self):
+        self.q.popleft()
+        self.persist()
