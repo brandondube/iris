a275e017e9a8fd604dcde7164bbffb0e012da779
Brandon Dube <brandondube@gmail.com>
2018-01-16 14:49:32 -0500

    persistent queue docstrings
    numpy format, need to convert rest of pyphase to numpy style docstrings

--- pyphase/persistent_queue.py
+++ pyphase/persistent_queue.py
@@ -1,42 +1,41 @@
-''' A queue object that persists to disk as a pickle with each operation.
-'''
+"""A queue object that persists to disk as a pickle with each operation."""
 import pickle
 from collections import deque
 
 
 class PersistentQueue(deque):
-    ''' A disk-persistent queue
+    """A disk-persistent queue.
 
-    Notes:
-        This object is not thread-safe and will have unexpected behavior if
-        shared between threads.
+    A deque-based object which functions like a FIFO queue and caches to
+    disk on each call. This object is not thread-safe and will have unexpected
+    behavior if shared between threads.
 
-        The intended / expected behavior is as follows:
-            - put() adds items to the end of the queue and persists it to disk.
+    The intended / expected behavior is as follows:
+        - put() adds items to the end of the queue and persists it to disk.
 
-            - the user will use peek() to nondestructively retrieve the top item
-                on the queue.  When a task using that data is complete,
-                mark_done() will be called, which will persist the queue to disk.
+        - the user will use peek() to nondestructively retrieve the top item
+            on the queue.  When a task using that data is complete,
+            mark_done() will be called, which will persist the queue to disk.
 
-            - use of get() destructively returns the first item from the queue and
-                persists it to disk.  Use this where the consuming code of that
-                object will not fail.
+        - use of get() destructively returns the first item from the queue and
+            persists it to disk.  Use this where the consuming code of that
+            object will not fail.
 
-    '''
+    Attributes:
+        path (pathlib.Path): path on disk of the queue
+        q (collections.deque): double ended queue object
+
+    """
+
     def __init__(self, path):
-        ''' Creates a new PersistentQueue, a deque-based object which functions
-            like a FIFO queue and caches to disk on each call.  The queue is not
-            thread safe and internally uses two deques to support safe failure
-            upon consumption of a piece of the queue.
+        """Create a new PersistentQueue.
 
-        Args:
-            path (`str`): where to persist the queue too.
+        Parameters
+        ----------
+        path : str
+            where to persist the queue too.
 
-        Returns:
-            PersistentQueue: a new persistent queue.
-
-        '''
-
+        """
         self.path = path
         try:
             with open(self.path, mode='rb') as file:
@@ -45,22 +44,41 @@ class PersistentQueue(deque):
             self.q = deque()
 
     def persist(self):
+        """Persist the queue to disk."""
         with open(self.path, mode='wb') as file:
             pickle.dump(self.q, file, protocol=-1)
 
     def put(self, item):
+        """Put an item on the end of the queue.
+
+        Args:
+            item (object): an item
+        """
         self.q.append(item)
         self.persist()
 
     def get(self):
+        """Return the leftmost item on the queue and remove it.
+
+        Returns:
+            object: leftmost item in queue.
+
+        """
         return self.q.popleft()
         self.persist()
 
     def peek(self):
+        """Return the leftmost item on the queue without removing it.
+
+        Returns:
+            object: leftmost item in queue.
+
+        """
         item = self.q.popleft()
         self.q.appendleft(item)
         return item
 
     def mark_done(self):
+        """Remove the leftmost item from the queue."""
         self.q.popleft()
         self.persist()
