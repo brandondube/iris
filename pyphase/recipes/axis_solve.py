6c59dc437eaf05d8cec6cb7330ef383c2f4bf504
Brandon Dube <brandondube@gmail.com>
2018-01-16 23:43:41 -0500

    restructure, update docstrings
    not currently functional as expected

--- pyphase/recipes/axis_solve.py
+++ pyphase/recipes/axis_solve.py
@@ -1,7 +1,6 @@
-''' Solve for aberrations on the optical axis given some truth MTF values
-    and lens parameters.
-'''
-from time import time
+"""Solve for aberrations on the optical axis given some truth MTF values and lens parameters."""
+import os
+import time
 from functools import partial
 from multiprocessing import Pool
 
@@ -18,7 +17,25 @@ from pyphase.forcefully_redirect_stdout import forcefully_redirect_stdout
 
 
 def grab_axial_data(setup_parameters, truth_dataframe):
-    # extract the axial T,S MTF data
+    """Pull axial through-focus MTF data from a pandas DataFrame.
+
+    Parameters
+    ----------
+    setup_parameters : dict
+        dictionary with keys `fno` and `wavelength`
+    truth_dataframe : pandas.DataFrame
+        Dataframe with columns `Field`, `Focus`, `Azimuth`, and `MTF`.
+
+    Returns
+    -------
+    wvfront_defocus : numpy.ndarray
+        array of defocus values in waves zero to peak.
+    ax_t : numpy.ndarray
+        array of tangential MTF values.
+    ax_s : numpy.ndarray
+        array of sagittal MTF values.
+
+    """
     s = setup_parameters
     axial_mtf_data = truth_dataframe[truth_dataframe.Field == 0]
     focuspos = np.unique(axial_mtf_data.Focus.as_matrix())
@@ -36,84 +53,150 @@ def grab_axial_data(setup_parameters, truth_dataframe):
     return wvfront_defocus, ax_t, ax_s
 
 
-def realize_focus_plane(setup_parameters, base_wavefront, t_true, s_true, defocus_wavefront):
+def realize_focus_plane(base_wavefront, t_true, s_true, defocus_wavefront):
+    """Compute the cost function for a single focal plane.
+
+    Parameters
+    ----------
+    base_wavefront : prysm.Pupil
+        a prysm Pupil object.
+    t_true : numpy.ndarray
+        array of true MTF values.
+    s_true : numpy.ndarray
+        array of true MTF values.
+    defocus_wavefront : Pupil
+        a prysm Pupil object.
+
+    Returns
+    -------
+    float
+        value of the cost function for this focus plane realization.
+
+    """
+    global setup_parameters
     prop_wvfront = base_wavefront + defocus_wavefront
     mtf = MTF.from_pupil(prop_wvfront, setup_parameters['efl'])
     t, s = mtf_ts_extractor(mtf, setup_parameters['freqs'])
     return mtf_cost_fcn(t_true, s_true, t, s)
 
 
-def optfcn(setup_parameters, defocus_pupils, tan, sag, pool, wavefrontcoefs):
+def optfcn(wavefrontcoefs):
+    """Optimization routine used to compare simulation data to measurement data.
+
+    Parameters
+    ----------
+    wavefrontcoefs : iterable
+        a vector of wavefront coefficients.
+
+    Returns
+    -------
+    float
+        cost function value
+
+    """
     # generate a "base pupil" with some aberration content
+    global setup_parameters, decoder_ring, pool, t_true, s_true, defocus_pupils
     s = setup_parameters
-    zdefocus, zsph3, zsph5, zsph7 = wavefrontcoefs
     efl, fno, wavelength, samples = s['efl'], s['fno'], s['wavelength'], s['samples']
-    pupil = FringeZernike(Z4=zdefocus, Z9=zsph3, Z16=zsph5, Z25=zsph7, base=1,
+    pupil_pass_zernikes = {key: value for (key, value) in zip(decoder_ring.values(), wavefrontcoefs)}
+    pupil = FringeZernike(**pupil_pass_zernikes, base=1,
                           epd=efl / fno, wavelength=wavelength, samples=samples)
 
     # for each focus plane, compute the cost function
-    rfp_mp = partial(realize_focus_plane,
-                     setup_parameters,
-                     pupil)
-    costfcn = pool.starmap(rfp_mp, zip(tan, sag, defocus_pupils))
+    rfp_mp = partial(realize_focus_plane, pupil)
+    costfcn = pool.starmap(rfp_mp, zip(t_true, s_true, defocus_pupils))
     return net_costfcn_reducer(costfcn)
 
 
-def optfcn_seq(setup_parameters, wvfront_defocus, tan, sag, wavefrontcoefs):
-    # generate a "base pupil" with some aberration content
-    s = setup_parameters
-    zdefocus, zsph3, zsph5, zsph7 = wavefrontcoefs
-    efl, fno, wavelength, samples = s['efl'], s['fno'], s['wavelength'], s['samples']
-    pupil = FringeZernike(Z4=zdefocus, Z9=zsph3, Z16=zsph5, Z25=zsph7, base=1,
-                          epd=efl / fno, wavelength=wavelength, samples=samples)
+def ready_pool(arg_dict):
+    """Initialize global variables inside process pool for windows support of shared read-only global state.
 
-    # for each focus plane, compute the cost function
-    costfcn = []
-    for tan, sag, defocus in zip(tan, sag, wvfront_defocus):
-        costfcn.append(realize_focus_plane(s, pupil, tan, sag, defocus))
-    return net_costfcn_reducer(costfcn)
+    Parameters
+    ----------
+    arg_dict : dict
+        dictionary of key/value pairs of variable names and values to expose at the global level.
+
+    """
+    globals().update(arg_dict)
 
 
-def sph_from_focusdiverse_axial_mtf(setup_parameters, truth_dataframe, guess=[0, 0, 0, 0]):
-    # extract the data
+def sph_from_focusdiverse_axial_mtf(sys_parameters, truth_dataframe, guess=(0, 0, 0, 0)):
+    """Retrieve spherical aberration-related coefficients from axial MTF data.
+
+    Parameters
+    ----------
+    sys_parameters : dict
+        dictionary with keys `efl`, `fno`, `wavelength`, `samples`.
+    truth_dataframe : pandas.DataFrame
+        a dataframe containing truth values.
+    guess : iterable, optional
+        guess coefficients for the wavefront.
+
+    Returns
+    -------
+    TYPE
+        Description
+
+    Raises
+    ------
+    Exception
+        Any exceptions raised by called functions are re-raised by this function.
+
+    """
+    # declare some state for this run as global variables to speed up access in multiprocess pool
+    global t_true, s_true, setup_parameters, decoder_ring, defocus_pupils, pool
+    setup_parameters = sys_parameters
     (focus_diversity,
      ax_t, ax_s) = grab_axial_data(setup_parameters, truth_dataframe)
 
     # casting ndarray to list makes it a list of arrays where the first index
     # is the focal plane and the second frequency.
-    ax_t, ax_s = list(ax_t), list(ax_s)
+    t_true, s_true = list(ax_t), list(ax_s)
 
     # precompute the defocus wavefronts to accelerate solving
     s = setup_parameters
+
     efl, fno, wvl, samples = s['efl'], s['fno'], s['wavelength'], s['samples']
+    decoder_ring = {
+        0: 'Z4',
+        1: 'Z9',
+        2: 'Z16',
+        3: 'Z25',
+    }
     defocus_pupils = []
     for focus in focus_diversity:
         defocus_pupils.append(Seidel(W020=focus, epd=efl / fno, wavelength=wvl, samples=samples))
-    pool = Pool()
-    optimizer_function = partial(optfcn,
-                                 setup_parameters,
-                                 defocus_pupils,
-                                 ax_t,
-                                 ax_s,
-                                 pool)
 
+    _globals = {
+        't_true': t_true,
+        's_true': s_true,
+        'setup_parameters': setup_parameters,
+        'decoder_ring': decoder_ring,
+        'defocus_pupils': defocus_pupils,
+    }
+    pool = Pool(processes=os.cpu_count(), initializer=ready_pool, initargs=[_globals])
+    optimizer_function = optfcn
     parameter_vectors = []
 
     def callback(x):
         parameter_vectors.append(x)
 
     try:
-        t_start = time()
+        t_start = time.perf_counter()
         # do the optimization and capture the per-iteration information from stdout
         with forcefully_redirect_stdout() as txt:
             result = minimize(
                 fun=optimizer_function,
                 x0=guess,
                 method='L-BFGS-B',
-                options={'disp': True},
+                options={
+                    'disp': True,
+                    'gtol': 1e-10,
+                    'ftol': 1e-12,
+                },
                 callback=callback)
 
-        t_end = time()
+        t_end = time.perf_counter()
         # grab the extra data
         cost_by_iter = parse_cost_by_iter_lbfgsb(txt.captured)
 
