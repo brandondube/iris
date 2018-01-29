d122ec85fda7fa432380a228ce4855ce4d5f15a2
Brandon Dube <brandondube@gmail.com>
2018-01-29 15:22:24 -0500

    update import + docstring

--- iris/recipes/__init__.py
+++ iris/recipes/__init__.py
@@ -1,4 +1,5 @@
-from pyphase.recipes.axis_solve import (
+"""Recipes for various solve types."""
+from iris.recipes.axis_solve import (
     grab_axial_data,
     sph_from_focusdiverse_axial_mtf,
 )
