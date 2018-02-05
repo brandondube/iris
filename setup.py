88a79eee9791d87898a31ff3e626e18589cb36d8
Brandon Dube <brandondube@gmail.com>
2018-02-04 23:49:01 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='iris',
-    version='0.0.10',
+    version='0.0.11',
     description='A python-based wavefront sensing module',
     long_description='',
     license='Copyright (C) 2017-2018 Brandon Dube, all rights reserved',
