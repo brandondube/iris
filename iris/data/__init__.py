feb4eb9cb5bcb7a013f8ddb694842c7978a5d63c
Brandon Dube <brandondube@gmail.com>
2018-02-11 14:00:27 -0500

    move pq under ./data, +./data/__init__

---
+++ iris/data/__init__.py
@@ -0,0 +1,8 @@
+"""Data shuffling utilities."""
+from iris.data.persistent_queue import PersistentQueue
+from iris.data.database import Database
+
+__all__ = [
+    'PersistentQueue',
+    'Database',
+]
