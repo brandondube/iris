d34ea76a89926aff35aceac307139d5924c098c0
Brandon Dube <brandondube@gmail.com>
2018-02-16 00:48:27 -0500

    fix import/export

--- iris/recipes/__init__.py
+++ iris/recipes/__init__.py
@@ -3,10 +3,12 @@ from iris.recipes.axis import (
     grab_axial_data,
 )
 from iris.recipes.main import (
-    opt_routine,
+    opt_routine_lbfgsb,
+    opt_routine_basinhopping,
 )
 
 __all__ = [
     'grab_axial_data',
-    'opt_routine',
+    'opt_routine_lbfgsb',
+    'opt_routine_basinhopping',
 ]
