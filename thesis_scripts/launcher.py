472a07709247af06f6ee930d525ccd3486df58a8
Brandon Dube <brandondube@gmail.com>
2018-02-23 15:12:39 -0500

    create optimizer arguments from config path

--- thesis_scripts/launcher.py
+++ thesis_scripts/launcher.py
@@ -7,8 +7,9 @@ import yaml
 
 from iris.data import PersistentQueue, Database
 from iris.worker import Worker
+from iris.core import _mtf_cost_core_diffractiondiv, _mtf_cost_core_sumsquarediff, _mtf_cost_core_manhattan
 
-cfgpath, cfgidx = sys.argv[1], sys.argv[2]
+cfgpath, cfgidx = sys.argv[1], int(sys.argv[2])
 with open(cfgpath) as fid:
     cfg = yaml.load(fid)
 
@@ -18,6 +19,21 @@ sets = list(product(names, nums))
 target = sets[cfgidx]
 
 if __name__ == '__main__':
+    s = target[0]
+    if s[0] == 'd':
+        diff = True
+    else:
+        diff = False
+    if s.split('_')[-1] == 'manhattan':
+        opt = _mtf_cost_core_manhattan
+    else:
+        opt = _mtf_cost_core_sumsquarediff
+
+    if diff:
+        optargs = (_mtf_cost_core_diffractiondiv, opt)
+    else:
+        optargs = (opt)
+
     p = Path(cfg['root'] / target[0] / target[1])
     q = PersistentQueue(p / 'queue.pkl')
     db = Database(p)
