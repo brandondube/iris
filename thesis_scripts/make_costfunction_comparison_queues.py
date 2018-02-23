20562e0638c7a2f3c0deb98f24c5afcbf73084f2
Brandon Dube <brandondube@gmail.com>
2018-02-23 15:06:28 -0500

    dual fudge to create more noise between z16, z25

--- thesis_scripts/make_costfunction_comparison_queues.py
+++ thesis_scripts/make_costfunction_comparison_queues.py
@@ -22,15 +22,15 @@ for stem in stems:
 
 # data
 np.random.seed(1234)
-fudge = np.random.normal(loc=1, scale=0.25, size=500)
+fudge1 = np.random.normal(loc=1, scale=0.5, size=500)
+fudge2 = np.random.normal(loc=1, scale=0.5, size=500)
+truths_z9 = np.random.random((500)) * .25
 z16_factor, z25_factor = -0.5, 0.5 ** 2
-truths_z9 = np.random.random((500))
-truths_z16, truths_z25 = truths_z9 * fudge * z16_factor, truths_z9 * fudge * z25_factor
+truths_z16, truths_z25 = truths_z9 * fudge1 * z16_factor, truths_z9 * fudge2 * z25_factor
 truths_z4 = np.zeros(truths_z9.shape)
 alltruth = np.stack((truths_z4, truths_z9, truths_z16, truths_z25), axis=1)
 segments = np.split(alltruth, 5, axis=0)
 
-# queues = [PersistentQueue(p) for p in q_paths]
 for outer in qs:
     for q, data in zip(outer, segments):
         d = list(data)
