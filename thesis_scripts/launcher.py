b47483dc40f9f4ac7039770e9ecc22b236f3b530
Brandon Dube <brandondube@gmail.com>
2018-03-06 22:32:20 -0500

    restructure launcher, add path guarantee

--- thesis_scripts/launcher.py
+++ thesis_scripts/launcher.py
@@ -9,33 +9,39 @@ from iris.data import PersistentQueue, Database
 from iris.worker import Worker
 from iris.core import _mtf_cost_core_diffractiondiv, _mtf_cost_core_sumsquarediff, _mtf_cost_core_manhattan
 
+# grab the config file path / name from the command line
 cfgpath, cfgidx = sys.argv[1], int(sys.argv[2])
 with open(cfgpath) as fid:
     cfg = yaml.load(fid)
 
+# build a list of path names and chunk numbers
 names, nums = cfg['names'], cfg['chunks']
 nums = [str(num) for num in nums]
 sets = list(product(names, nums))
 target = sets[cfgidx]
 
+# construct optimization directives
+s = target[0]
+if s[0] == 'd':
+    diff = True
+else:
+    diff = False
+if s.split('_')[-1] == 'manhattan':
+    opt = _mtf_cost_core_manhattan
+else:
+    opt = _mtf_cost_core_sumsquarediff
+
+if diff:
+    optargs = (_mtf_cost_core_diffractiondiv, opt)
+else:
+    optargs = (opt)
+
+# shield for multiprocessing
 if __name__ == '__main__':
-    s = target[0]
-    if s[0] == 'd':
-        diff = True
-    else:
-        diff = False
-    if s.split('_')[-1] == 'manhattan':
-        opt = _mtf_cost_core_manhattan
-    else:
-        opt = _mtf_cost_core_sumsquarediff
-
-    if diff:
-        optargs = (_mtf_cost_core_diffractiondiv, opt)
-    else:
-        optargs = (opt)
-
-    p = Path(cfg['root'] / target[0] / target[1])
-    q = PersistentQueue(p / 'queue.pkl')
-    db = Database(p)
-    w = Worker(q, db, optmode='global', optparallel=True, optnumthreads=23, work_time=60 * 7)
+    root = Path(cfg['root'])
+    root.mkdir(parents=True, exist_ok=True)
+    base = root / target[0] / target[1])
+    q = PersistentQueue(base / 'queue.pkl')
+    db = Database(base)
+    w = Worker(q, db, optmode='global', optopts={'parallel': True, 'nthreads': 23}, work_time=60 * 7)
     w.start()
