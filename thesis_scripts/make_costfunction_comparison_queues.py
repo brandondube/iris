563000f97654bf38bfb9b5b54085b14a3b3a2989
Brandon Dube <brandondube@gmail.com>
2018-03-06 21:58:43 -0500

    more explained rng for truth creation

--- thesis_scripts/make_costfunction_comparison_queues.py
+++ thesis_scripts/make_costfunction_comparison_queues.py
@@ -20,14 +20,22 @@ for stem in stems:
         counter += 1
     qs.append(local)
 
-# data
+# set the seed and generate 'fudge factor' arrays centered on 1 with stdev 1/2 and 500 samples e/a
 np.random.seed(1234)
 fudge1 = np.random.normal(loc=1, scale=0.5, size=500)
 fudge2 = np.random.normal(loc=1, scale=0.5, size=500)
-truths_z9 = np.random.random((500)) * .25
 z16_factor, z25_factor = -0.5, 0.5 ** 2
+
+# no defocus
+# uniform normal distributions of size 500 and range [0, 0.25]
+truths_z4 = np.zeros(500)
+truths_z9 = np.random.random(truths_z4.shape) * .25
+
+# Z16 and Z25 are derived from Z9 and have value -1/2 and +1/4 respectively
+# but contain gaussian noise that makes this relationship fuzzy
 truths_z16, truths_z25 = truths_z9 * fudge1 * z16_factor, truths_z9 * fudge2 * z25_factor
-truths_z4 = np.zeros(truths_z9.shape)
+
+# stack all truths together and split them into 5 chunks for 5 workers per datatype
 alltruth = np.stack((truths_z4, truths_z9, truths_z16, truths_z25), axis=1)
 segments = np.split(alltruth, 5, axis=0)
 
