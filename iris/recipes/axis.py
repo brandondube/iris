80a88633d0f15fd3bf23b6754339cab3601444c8
Brandon Dube <brandondube@gmail.com>
2018-03-11 13:20:08 -0400

    clean up dead code

--- iris/recipes/axis.py
+++ iris/recipes/axis.py
@@ -1,47 +1,8 @@
 """Solve for aberrations on the optical axis given some truth MTF values and lens parameters."""
-from itertools import product
 
 import numpy as np
 
 from prysm.thinlens import image_displacement_to_defocus
-
-
-def generate_axial_truth_coefs(max_val, num_steps, symmetric=True):
-    """Generate a cartesian product of W040, W060, and W080.
-
-    Subject to a best focus constraint for minimum RMS wavefront error. Note that for a large
-    num_steps, the output will be large; num_steps=10 will produce 1,000 items.
-
-    Parameters
-    ----------
-    max_val : `int` or `float`
-        maximum value of each coefficient
-    num_steps : `int`
-        number of points between bounds
-    symmetric : bool, optional
-        whether the bounds are symmetric (negative to positive) or not (0 to positive)
-
-    Returns
-    -------
-    list
-        list of arrays that are truth values for each of Z4, Z9, Z16, Z25
-
-    """
-    if symmetric is True:
-        lower = -max_val
-    else:
-        lower = 0
-
-    w040 = np.linspace(lower, max_val, num_steps)
-    w060 = np.linspace(lower, max_val, num_steps)
-    w080 = np.linspace(lower, max_val, num_steps)
-
-    # take the product
-    coefs = list(product(w040, w060, w080))
-
-    # here, figure out what w020 is for best focus and add it to the
-    # coefficients
-    return coefs
 
 
 def generate_random_axial_truth_coefs(peak, ncoefs, symmetric=True):
