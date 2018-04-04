daf40a9cf50e05c54d39f7a20a3fb098639d04a6
Brandon Dube <brandondube@gmail.com>
2018-04-04 19:29:06 -0400

    faster defaults

--- iris/recipes/main.py
+++ iris/recipes/main.py
@@ -100,7 +100,7 @@ def opt_routine_lbfgsb(sys_parameters, truth_dataframe, codex, guess=(0, 0, 0, 0
 
 
 def opt_routine_basinhopping(sys_parameters, truth_dataframe, codex, guess=(0, 0, 0, 0),
-                             ftol=1e-7, step=0.05, temp=0.05, max_starts=100,
+                             ftol=1e-7, step=0.05, temp=0.05, max_starts=25,
                              parallel=False, nthreads=None, core_opts=None):
     """Pseudoglobal basin-hopping based optimization routine.
 
@@ -197,7 +197,7 @@ def opt_routine_basinhopping(sys_parameters, truth_dataframe, codex, guess=(0, 0
                     'options': {
                         'disp': True,
                         'ftol': ftol,
-                        'maxiter': 100,
+                        'maxiter': 50,
                     },
                     'callback': cb_local,
                 },
