dfc57f1b3299b5f8af9c5b017db29da89f87e3db
Brandon Dube <brandondube@gmail.com>
2018-04-23 01:10:57 -0400

    cleanup for one-shot usage

--- thesis_scripts/launcher_cma.py
+++ thesis_scripts/launcher_cma.py
@@ -1,35 +1,25 @@
 """Launches a worker with the appropriate queue and database based on an index into a config file."""
-import sys
 from pathlib import Path
 
-import yaml
-
 from iris.data import PersistentQueue, Database
-from iris.rings import W3
+from iris.rings import W2
 from iris.worker import Worker
 
-# grab the config file path / name from the command line
-cfgpath, cfgidx = sys.argv[1], int(sys.argv[2])
-with open(cfgpath) as fid:
-    cfg = yaml.load(fid)
-
-chunk = cfg['chunks'][cfgidx]
 
 # shield for multiprocessing
 if __name__ == '__main__':
-    root = Path(cfg['root']).expanduser()
-    base = root / str(chunk)
-    q = PersistentQueue(base / 'queue.pkl')
-    db = Database(base)#, fields=['truth_rmswfe', 'cost_first', 'cost_final', 'rrmswfe_final', 'time', 'nit'])
+    root = Path(__file__).parent / 'data' / 'coma-vs-angle'
+    q = PersistentQueue(root / 'queue.pkl')
+    db = Database(root, fields=['truth_rmswfe', 'cost_first', 'cost_final', 'rrmswfe_final', 'time', 'nit'])
     w = Worker(q,
                db,
                optmode='global',
                simopts={
-                   'guess': [0] * 18,
-                   'decoder_ring': W3
+                   'guess': [0] * 16,
+                   'decoder_ring': W2
                    },
                optopts={
-                   'parallel': True,
+                   'parallel': False,
                    'nthreads': 7,
                    'ftol': 1e-7,
                },
