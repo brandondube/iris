1906630552177e8f6ee4684126341ca89f5e19f1
Brandon Dube <brandondube@gmail.com>
2018-04-22 21:24:50 -0400

    correct errors in coma_astigmatism queues

--- thesis_scripts/make_coma_astigmatism_queue.py
+++ thesis_scripts/make_coma_astigmatism_queue.py
@@ -7,7 +7,7 @@ from iris.data import PersistentQueue
 
 spherical_amounts = np.asarray([0, 0.05, 0.1, 0.2])
 coma_amounts = np.asarray([0.05, 0.1, 0.2])
-angles = np.radians(np.linspace(0, 90, 18))
+angles = np.radians(np.linspace(0, 90, 19))
 
 sets = product(spherical_amounts, coma_amounts)
 sph_amounts_tiled, coma_amounts_tiled = [], []
@@ -19,35 +19,34 @@ s = len(coma_amounts_tiled)
 
 sph_amounts_tiled, coma_amounts_tiled = np.asarray(sph_amounts_tiled), np.asarray(coma_amounts_tiled)
 
-sphcoef = (np.ones(18) * sph_amounts_tiled.reshape((s, 1))).ravel()
+sphcoef = (np.ones(angles.shape) * sph_amounts_tiled.reshape((s, 1))).ravel()
 xcoef, ycoef = np.cos(angles), np.sin(angles)  # flipped -- optics convention
 xcoef = (xcoef * coma_amounts_tiled.reshape((s, 1))).ravel()
 ycoef = (ycoef * coma_amounts_tiled.reshape((s, 1))).ravel()
 
-out = np.zeros((sphcoef.shape[0], 16))
-out[:, 5] = sphcoef
-out[:, 10] = sphcoef / -2
-out[:, 15] = sphcoef / 4
-out[:, 3] = xcoef
-out[:, 4] = xcoef
+out_coma = np.zeros((sphcoef.shape[0], 16))
+out_coma[:, 5] = sphcoef
+out_coma[:, 10] = sphcoef / -2
+out_coma[:, 15] = sphcoef / 4
+out_coma[:, 3] = xcoef
+out_coma[:, 4] = ycoef
+
+out_astig = np.zeros((sphcoef.shape[0], 16))
+out_astig[:, 5] = sphcoef
+out_astig[:, 10] = sphcoef / -2
+out_astig[:, 15] = sphcoef / 4
+out_astig[:, 1] = xcoef
+out_astig[:, 2] = ycoef
+
+root = Path(__file__).parent / 'data' / 'coma-vs-angle'
+p = root / 'queue.pkl'
+q = PersistentQueue(p, overwrite=True)
+items = list(out_coma)
+q.put_many(items)
+
 
 root = Path(__file__).parent / 'data' / 'astigmatism-vs-angle'
 p = root / 'queue.pkl'
 q = PersistentQueue(p, overwrite=True)
-items = list(out)
+items = list(out_astig)
 q.put_many(items)
-#chunks = np.split(out, 4)
-
-# root = Path(__file__).parent / '..' / '..' / 'data'
-# rootc = root / 'coma-vs-angle'
-# roota = root / 'astigmatism-vs-angle'
-# for idx, chunk in enumerate(chunks):
-#     p = rootc / str(idx) / 'queue.pkl'
-#     q = PersistentQueue(p, overwrite=True)
-#     items = list(chunk)
-#     q.put_many(items)
-#
-#     p = roota / str(idx) / 'queue.pkl'
-#     q = PersistentQueue(p, overwrite=True)
-#     items = list(chunk)
-#     q.put_many(items)
